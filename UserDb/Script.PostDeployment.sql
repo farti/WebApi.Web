3971425696d459845b0e2be40d2506b0bfe95b40
mrakoczy <mrakoczy@umbitious.com>
2022-05-24 09:59:24 +0200

    Add db

---
+++ UserDb/Script.PostDeployment.sql
@@ -0,0 +1,9 @@
+﻿IF not exists (SELECT 1 FROM dbo.[User])
+
+BEGIN
+	insert into dbo.[User] (FirstName,LastName)
+	VALUES ('Marcin', 'Rakoczy'),
+	('Stefan', 'Śliwka'),
+	('Jola','Gruszka'),
+	('Adam', 'Pomelo')
+END
---
+++ UserDb/StoredProcedures/spUser_GetAll.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spUser_GetAll]
+
+AS
+	BEGIN
+		SELECT *
+		FROM dbo.[User];
+	END
